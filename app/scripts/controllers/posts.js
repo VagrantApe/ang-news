04d1b3c3f47b004b83d26bbd3b0c218f294658f7
John Johnson <luckstedcraig@gmail.com>
2014-08-11 11:45:17 -0500

    5th ch

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -4,12 +4,6 @@ app.controller('PostsCtrl', function($scope, $location, Post) {
 
   $scope.posts=Post;
 
-  $scope.submitPost = function(){
-    $scope.post.url = 'http://'+$scope.post.url;
-    Post.$add($scope.post);
-    $scope.post = {title:'', url:''};
-  };
-
   $scope.deletePost = function(postId){
     Post.$remove(postId);
   };
